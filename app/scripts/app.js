2643fbc6059c330fa3941e36ba19bb8c5478c925
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-04-14 10:24:29 -0500

    added index.html and app.js with initialized firebase

---
+++ app/scripts/app.js
@@ -0,0 +1,10 @@
+//Initialize firebase
+var config = {
+    apiKey: "AIzaSyAlIQXZuKYVaIenuWdlS2k2kckv8TUsxbM",
+    authDomain: "cookbook-c1458.firebaseapp.com",
+    databaseURL: "https://cookbook-c1458.firebaseio.com",
+    projectId: "cookbook-c1458",
+    storageBucket: "cookbook-c1458.appspot.com",
+    messagingSenderId: "541654605084"
+  };
+  firebase.initializeApp(config);
