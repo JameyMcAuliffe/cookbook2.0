4f768245537ffcf627868772b17685ed57df6a32
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-05-05 11:01:56 -0500

    basic angular setup, home template renders

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,25 @@
-//Initialize firebase
-var config = {
-    apiKey: "AIzaSyAlIQXZuKYVaIenuWdlS2k2kckv8TUsxbM",
-    authDomain: "cookbook-c1458.firebaseapp.com",
-    databaseURL: "https://cookbook-c1458.firebaseio.com",
-    projectId: "cookbook-c1458",
-    storageBucket: "cookbook-c1458.appspot.com",
-    messagingSenderId: "541654605084"
-  };
-  firebase.initializeApp(config);
 
-const dbRef = firebase.database().ref().child('recipes')
-dbRef.on('value', snap => console.log(snap.val()))
+const app = angular.module('Cookbook', ['ngRoute'])
+	.config(function($routeProvider) {
 
+	//Initialize firebase
+	var config = {
+	    apiKey: "AIzaSyAlIQXZuKYVaIenuWdlS2k2kckv8TUsxbM",
+	    authDomain: "cookbook-c1458.firebaseapp.com",
+	    databaseURL: "https://cookbook-c1458.firebaseio.com",
+	    projectId: "cookbook-c1458",
+	    storageBucket: "cookbook-c1458.appspot.com",
+	    messagingSenderId: "541654605084"
+	  };
+	  firebase.initializeApp(config);
+
+	const dbRef = firebase.database().ref().child('recipes')
+	dbRef.on('value', snap => console.log(snap.val()))
+
+	//Routes
+	$routeProvider
+		.when('/', {
+			templateUrl: '../templates/home.html'
+		})
+
+})
