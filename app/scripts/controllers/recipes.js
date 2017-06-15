4cad9c5ccab8ba2854cfe17a4571b2831903f539
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-06-15 14:25:39 -0500

    pulling out data from fb in wanted format

--- app/scripts/controllers/recipes.js
+++ app/scripts/controllers/recipes.js
@@ -3,7 +3,18 @@
 app.controller('RecipesCtrl', function($scope, Recipes) {
 	Recipes.getRecipes()
 	.then(recipes => {
-		$scope.recipes = recipes
-		console.log("Recipes:", $scope.recipes)
+		$scope.recipes = Object.keys(recipes)
+		$scope.ingredients = Object.values(recipes)
+		$scope.ingredientNames
+		$scope.ingredientValues
+		for(let i = 0; i < $scope.ingredients.length; i++) {
+			$scope.ingredientNames = (Object.keys($scope.ingredients[i].Ingredients))
+			$scope.ingredientValues = (Object.values($scope.ingredients[i].Ingredients))
+		}
+		console.log("Keys:", Object.keys(recipes))
+		console.log("Values:", Object.values(recipes))
+		console.log("Ingredients:", $scope.ingredientNames)
+		// console.log("Ingredients:", Object.keys($scope.ingredients[0].Ingredients))
+		// console.log("Ingredients:", Object.values($scope.ingredients[0].Ingredients))
 	 })
 })
--- app/scripts/factories/recipes-db.js
+++ app/scripts/factories/recipes-db.js
@@ -2,13 +2,8 @@
 
 app.factory('Recipes', function($http) {
 	const getRecipes = () => {
-		// return $http.get('https://console.firebase.google.com/project/cookbook-c1458/database/data.json')
 		return $http.get('https://cookbook-c1458.firebaseio.com/recipes.json')
 		.then(data => data.data)
-		// const dbRef = firebase.database().ref().child('recipes')
-		// return dbRef.on('value', snap => console.log(snap.val()))
-
-
 	}
 
 	return { getRecipes }
